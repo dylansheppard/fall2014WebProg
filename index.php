be7954551166e339b3753070c22a9b6a6eb24b8f
unknown <n02395809@LC112PC26.active.newpaltz.edu>
2014-08-28 10:37:34 -0400

    Our first commit in Apatana

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+
+		<title>First File!</title>
+		
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>new_file</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by n02395809
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
