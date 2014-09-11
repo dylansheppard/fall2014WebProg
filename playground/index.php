41eda6b8ed36c68a8ea2aa54f293d58db9de3c33
Dylan Sheppard <nyyankees007@client172-80.wireless.newpaltz.edu>
2014-09-11 08:35:41 -0400

    html layouts

--- playground/index.php
+++ playground/index.php
@@ -1,36 +1,190 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		
+<!doctype html>
+<html>
+<head>
+	<title>Fitness Tracker</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<style type = "text/css">
 
-		<title>This was edited in Cloud 9! D:</title>
-		
-	</head>
 
-	<body>
-		<div>
-			<header>
-				<h1>HELLO HELLO HELLO</h1>
-				<h2> Today is a good day.</h2>
-			</header>
-			<nav>
-				<p>
-					<a href="/">Home</a>
-				</p>
-				<p>
-					<a href="/contact">Contact</a>
-				</p>
-			</nav>
+.content {
+	min-height: 100%;
+	position: relative;
+	overflow: auto;
+	z-index: 0; 
+}
 
-			<div>
+.background {
+	position: absolute;
+	z-index: -1;
+	top: 0;
+	bottom: 0;
+	margin: 0;
+	padding: 0;
+}
 
-			</div>
+.top_block {
+	width: 100%;
+	display: block; 
+}
 
-			<footer>
-				<p>
-					&copy; Copyright  by n02395809
-				</p>
-			</footer>
+.bottom_block {
+	position: absolute;
+	width: 100%;
+	display: block;
+	bottom: 0; 
+}
+
+.left_block {
+	display: block;
+	float: left; 
+}
+
+.right_block {
+	display: block;
+	float: right; 
+}
+
+.center_block {
+	display: block;
+	width: auto; 
+}
+
+.logo {
+	width: 100%;
+	height: 150px;
+	background-color: #9999ff;
+}
+
+.dropdown {
+	width: 100%;
+	height: 45px;
+	background-color: #9966ff;
+	margin-bottom: 100px;
+}
+
+.features {
+	width: 25%;
+	height: 250px;
+	background-color: #996666;
+	margin-left: 80px;
+	margin-top: 20px;
+	position: relative;
+	float: left;
+	display: block;
+	text-align: center;
+	
+}
+
+.features2 {
+	width: 56%;
+	height: 250px;
+	background-color: #996666;
+	margin-left: 80px;
+	margin-top: 20px;
+	position: relative;
+	float: left;
+	display: block;
+	text-align: center;
+	
+}
+
+#signup {
+	postion: relative;
+	height: 300px;
+	width: 80%;
+	background-color: #CCC;
+	float: left;
+	display: block;
+	margin-left: 125px;
+	margin-right: 100px;
+	padding-left: 30px;
+}
+
+.button1 {
+	postion: relative;
+	width: 20%;
+	height: 80px;
+	background-color: #666;
+	float: left;
+	display: block;
+	border-radius: 10px;
+	margin-top: 125px;
+	text-align: center;
+	padding-top: 20px;
+	
+}
+
+</style>
+</head>
+
+
+<body>
+	<div class="content">
+		<div class="top_block logo">
+        	<p> logo will go here, dropdown menu immeditely beneath me. </p>
+				
 		</div>
-	</body>
+		
+        <div class="top_block dropdown">
+     
+			<div class="content">
+			</div>
+			 <div id = "signup">
+             	
+                <p> Cool sign-up banner will go here. </p>
+            
+             <div class = "button1">
+             <a href ="http://cs.newpaltz.edu/~n02395809/signup.php">SIGN UP</a>
+             
+             </div>
+    
+    </div>
+		</div>
+		
+		
+	<div class = "features">
+    	<p> Some cool pics will go here. </p>
+    </div>
+    
+    <div class = "features">
+    	<p> TEST </p>
+    </div>
+    
+    <div class = "features">
+    	<p> Images will go here. </p>
+    </div>
+    
+    <div class = "features">
+    	<p> Text text text  </p>
+    </div>
+    
+    <div class = "features">
+    	<p> REASONS TO JOIN </p>
+    </div>
+    
+    <div class = "features">
+    	<p> LOOK AT ALL THESE COOL FEATURES </p>
+    </div>
+    
+    <div class = "features">
+    	<p> One last image here </p>
+    </div>
+    
+    <div class = "features2">
+    	<p> This will be an extended, very heartfelt description of the wonderment of this fitness tracker. Perhaps a user testimony will go here... someday.
+    	</p>
+    </div>
+    
+   
+			</div>
+		</div>
+	</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+
 </html>
+
---
+++ playground/signup.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html>
+<head>
+	<title>Sign up</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<style type = "text/css">
+
+
+.content {
+	min-height: 100%;
+	position: relative;
+	overflow: auto;
+	z-index: 0; 
+}
+
+.background {
+	position: absolute;
+	z-index: -1;
+	top: 0;
+	bottom: 0;
+	margin: 0;
+	padding: 0;
+}
+
+.top_block {
+	width: 100%;
+	display: block; 
+}
+
+.bottom_block {
+	position: absolute;
+	width: 100%;
+	display: block;
+	bottom: 0; 
+}
+
+.left_block {
+	display: block;
+	float: left; 
+}
+
+.right_block {
+	display: block;
+	float: right; 
+}
+
+.center_block {
+	display: block;
+	width: auto; 
+}
+
+.logo {
+	width: 100%;
+	height: 150px;
+	background-color: #9999ff;
+}
+
+.dropdown {
+	width: 100%;
+	height: 45px;
+	background-color: #9966ff;
+	margin-bottom: 100px;
+}
+
+.signupForm {
+	width: 35%;
+	height: 550px;
+	padding: 40px;
+    margin-left: 70px;
+	margin-right: 20px;
+	margin-top: 50px;
+	margin-bottom: 200px;
+	background-color: #36F;
+	position: relative;
+	display: block;
+	float: left;
+	
+	
+	
+}
+
+
+
+
+
+.button1 {
+	postion: relative;
+	width: 20%;
+	height: 80px;
+	background-color: #666;
+	float: left;
+	display: block;
+	border-radius: 10px;
+	margin-top: 125px;
+	text-align: center;
+	padding-top: 20px;
+	
+}
+
+</style>
+</head>
+
+
+<body>
+	<div class="content">
+		<div class="top_block logo">
+        	<p> logo will go here, dropdown menu immeditely beneath me. </p>
+				
+		</div>
+		
+        <div class="top_block dropdown">
+     
+			<div class="content">
+         
+            </div>
+               <div class = "signupForm">
+            <h2> SIGN UP FORM </h2>
+            <form>
+				First name: <input type="text" name="firstname"><br>
+                <br>
+				Last name: <input type="text" name="lastname">
+                <br> <br>
+                Username: <input type ="text" name="username"> 
+                <br> <br>
+                Password: <input type ="password" name="password"> 
+                <br> <br>
+                Retype PW: <input type ="password" name="password"> 
+                <br> <br>
+                 Email Address: <input type ="password" name="password"> 
+                <br> <br>
+                
+                <input type="submit" value="Submit">
+</form> 
+                
+                
+                
+			</form>
+            
+			</div>
+            
+                  <div class = "signupForm">
+            <h4> This will be another banner displaying feautures and happy users.</h4>
+           
+            
+			</div>
+			
+
+    </div>
+    
+   
+			</div>
+	
+
+</body>
+
+
+</html>
