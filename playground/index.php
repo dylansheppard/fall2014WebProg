22ebc3fcf01340b37714753e42c040c4969dfae5
unknown <n02395809@LC112PC25.active.newpaltz.edu>
2014-09-15 10:46:15 -0400

    edits

--- playground/index.php
+++ playground/index.php
@@ -16,70 +16,6 @@
 
 
 
-.logo {
-	width: 100%;
-	height: 150px;
-	background-color: #9999ff;
-}
-
-.dropdown {
-	width: 100%;
-	height: 45px;
-	background-color: #9966ff;
-	margin-bottom: 100px;
-}
-
-.features {
-	width: 25%;
-	height: 250px;
-	background-color: #996666;
-	margin-left: 80px;
-	margin-top: 20px;
-	position: relative;
-	float: left;
-	display: block;
-	text-align: center;
-	
-}
-
-.features2 {
-	width: 56%;
-	height: 250px;
-	background-color: #996666;
-	margin-left: 80px;
-	margin-top: 20px;
-	position: relative;
-	float: left;
-	display: block;
-	text-align: center;
-	
-}
-
-#signup {
-	postion: relative;
-	height: 300px;
-	width: 80%;
-	background-color: #CCC;
-	float: left;
-	display: block;
-	margin-left: 125px;
-	margin-right: 100px;
-	padding-left: 30px;
-}
-
-.button1 {
-	postion: relative;
-	width: 20%;
-	height: 80px;
-	background-color: #666;
-	float: left;
-	display: block;
-	border-radius: 10px;
-	margin-top: 125px;
-	text-align: center;
-	padding-top: 20px;
-	
-}
 
 
 
@@ -91,7 +27,7 @@
 	<div class="container">
 		<ul class="nav nav-tabs" role="tablist">
   			<li class="active"><a href="#">Home</a></li>
- 			 <li><a href="#">Nutrition</a></li>
+ 			 <li><a href="food.php">Nutrition</a></li>
   			<li><a href="#">Exercise</a></li>
   			<li><a href="#">Blog</a></li>
   				<li><a href="#">My Profile</a></li>
@@ -118,7 +54,7 @@
     <div class="row">
   	<div class="col-md-4">
     	<div class="thumbnail">
-      		<img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/1601293_10153925389432355_1433297507_n.jpg?oh=d7ff1139df3076d4b74e889ac8558fd2&oe=54CD8581&__gda__=1418004315_c3b8664a3e4d1707cb3a5255024fc241" style = "width:300px; height:300px" alt="...">
+      		<img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/1601293_10153925389432355_1433297507_n.jpg?oh=d7ff1139df3076d4b74e889ac8558fd2&oe=54CD8581&__gda__=1418004315_c3b8664a3e4d1707cb3a5255024fc241" class = "img-responsive img-circle" style = "width:300px; height:300px" alt="...">
       		<!-- normally, i'd resize images before I place them in, I'm just trying to have some fun with a rather dry layout assignment. Please excuse the random in HTML CSS for now! --> 
       		<div class="caption">
         		<h3>Featured User: Greg Denise</h3>
@@ -133,7 +69,7 @@
 	
   	<div class="col-md-4">
     	<div class="thumbnail">
-      		<img src="http://placehold.it/300.png/09f/fff" alt="...">
+      		<img src="http://placehold.it/300.png/09f/fff" class = "img-responsive img-circle" alt="...">
       		<div class="caption">
         		<h3>It's totally free!</h3>
         			<p>Wow it's free! Free free free free free free free free free free  Free free free free free free free free free free  Free free free free free free free free free free  Free free free free free free free free free free
@@ -147,7 +83,7 @@
 	
   	<div class="col-md-4">
     	<div class="thumbnail">
-      		<img src="http://placehold.it/300.png/09f/fff" alt="...">
+      		<img src="http://placehold.it/300.png/09f/fff" class = "img-responsive img-circle" alt="...">
       		<div class="caption">
         		<h3>FEATURES</h3>
         			<ul type = "square">
@@ -171,6 +107,13 @@
 		</div>
 	</div>
 
+	<footer>
+				<div class="container">
+					<p>
+						&copy; Copyright  by Dylan Sheppard
+					</p>
+				</div>
+			</footer>
 </body>
 
 
